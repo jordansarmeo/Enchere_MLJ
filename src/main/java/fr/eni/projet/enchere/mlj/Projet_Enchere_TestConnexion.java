bfa04521d854e8af06c3fe86e595a95f58c68241
weaveul <weveul@live.fr>
2022-02-15 11:06:52 +0100

    Class test connection sql

--- src/main/java/fr/eni/projet/enchere/mlj/Projet_Enchere_TestConnexion.java
+++ src/main/java/fr/eni/projet/enchere/mlj/Projet_Enchere_TestConnexion.java
@@ -12,16 +12,11 @@ public class Projet_Enchere_TestConnexion
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("1");
             Connection con=DriverManager.getConnection(
                     "jdbc:sqlserver://127.0.0.1/PROJET_ENCHERE","user_enchere","1Aec5ff2cf");
-            System.out.println("2");
-            Statement stmt=con.createStatement();
-            System.out.println("3");
+            Statement stmt=con.createStatement();  
             ResultSet rs=stmt.executeQuery("show databases;");
-            System.out.println("4");
             System.out.println("Connected");  
-            System.out.println("5");
         }
         catch(Exception e)
         {

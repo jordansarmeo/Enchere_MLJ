1393fd41c468babe68483b76f47b21c686e25de3
Monika <Nadhem@nadhem>
2022-02-17 16:42:17 +0100

    mise a jour

--- src/main/java/fr/eni/projet/enchere/mlj/servlets/ServletValidantUtilisateur.java
+++ src/main/java/fr/eni/projet/enchere/mlj/servlets/ServletValidantUtilisateur.java
@@ -26,7 +26,7 @@ public class ServletValidantUtilisateur extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	 
+		getServletContext().getRequestDispatcher("/pageProfilUtilisateur.html").forward(request, response);
 		
 		
 		 
